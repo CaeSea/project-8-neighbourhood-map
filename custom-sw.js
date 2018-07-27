e17171208858a02edc74da8a7309d5e7d601b169
Caeron Seabourne <caeking93@gmail.com>
2018-07-27 15:55:26 +0100

    Updates

--- custom-sw.js
+++ custom-sw.js
@@ -14,19 +14,15 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  //console.log(event.request);
+  console.log(event.request);
   event.respondWith(
-    new Response('hi')
-  );
-    /*
     caches.match(event.request).then(function(response) {
       if(response) return response;
       return fetch(event.request);
     }).catch(function() {
       console.log('Error with fetch request.');
     })
-    */
-
+  )
 });
 
 
